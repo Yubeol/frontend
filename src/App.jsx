5bebecd84f196a5ada98f6ea3a9b00a9e7f8ad08
Yubeol <minkyu.ryu00@gmail.com>
2026-05-19 17:26:16 +0900

    app.jsx HeaderBar 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './no1_pages/HomePage'
 import TodoPage from './no1_pages/TodoPage'
 import EmployeePage from './no1_pages/EmployeePage'
+import SiderBar from './no2_components/layout/SiderBar'
+import HeaderBar from './no2_components/layout/HeaderBar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Routes>
+      <HeaderBar/>
+      <div>
+        <SiderBar/>
+      </div>
+        <div>
+          <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/todo" element={<TodoPage/>}/>
           <Route path="/employee" element={<EmployeePage/>}/>
         </Routes> 
+        </div>
       </BrowserRouter>
       
     </>
--- src/no2_components/layout/SiderBar.jsx
+++ src/no2_components/layout/SiderBar.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const SiderBar = () => {
-  return (
-    <div>
-        <div>
-            <Link to="/">Home</Link>
-        </div>
-        <div>
-            <Link to="/">Home</Link>
-        </div>
-        <div>
-            <Link to="/">Home</Link>
-        </div>
-    </div>
-  )
+    return (
+        <aside>
+            <div>
+                <div>
+                    <Link to="/">Home</Link>
+                </div>
+                <div>
+                    <Link to="/todo">할일</Link>
+                </div>
+                <div>
+                    <Link to="/employee">고용인 정보</Link>
+                </div>
+            </div>
+        </aside>
+
+    )
 }
 
 export default SiderBar
