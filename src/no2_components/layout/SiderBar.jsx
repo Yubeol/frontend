d889822458419df959185eda76cf3b1c013408e8
Yubeol <minkyu.ryu00@gmail.com>
2026-05-19 17:16:44 +0900

    first

---
+++ src/no2_components/layout/SiderBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const SiderBar = () => {
+  return (
+    <div>
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+    </div>
+  )
+}
+
+export default SiderBar
